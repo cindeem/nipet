db3ceb1ac9e32f7020abcea9f9b6865f1d334567
CindeeM <cindee@berkeley.edu>
2012-12-03 11:45:16 -0800

    added initial setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+
+
+from distutils.core import setup
+
+setup(
+    name='nipet',
+    version='0.2.0',
+    author='Cindee Madison'
+    author_email='cindeem at gmail dot com"
+    packages=['nipet', 'nipet.test'],
+    license='LICENSE.txt',
+    description='Tools for Graphical Analysis of PET data',
+    long_description=open('README.txt').read(),
+    install_requires=[
+        "numpy >= 1.6.1",
+        ],
+)
