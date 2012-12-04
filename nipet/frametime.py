f68376bd86f37e57c5b195937bb206beed364680
Caleb Wang <cw@berkeley.edu>
2012-12-04 12:44:32 -0800

    Refactored frametime.py to use numpy.loadtxt. Added frametime.delete_frame. Removed deprecated helper methods

--- nipet/frametime.py
+++ nipet/frametime.py
@@ -1,27 +1,41 @@
-import numpy as np
 import csv
+import numpy as np
+from pandas import ExcelFile, readcsv
 
 class FrameTime:
     """
-    Generates timing file for use with graphical analysis.
+    Reads or generates timing file for use with graphical analysis.
     Format (number of frames X 4):
         frame_number, start_time, stop_time, duration    
     """ 
     def __init__(self):
-        self.data_size = 4
-        self.clear()
+        """
+        col_num 
+            number of columns
+        """
+        self.col_num = 4
 
     def isempty(self):
-        return self.data == np.array([[0], [0], [0], [0]])     
+        if self.data:
+            return False
+        return True    
 
-    def clear(self):
-        self.data = np.array([[0], [0], [0], [0]])     
+    def delete_frame(self, frame):
+        """Delete a frame.
+        Currently deletes based on position in array.
+        Should eventually delete based on frame number."""
+        self.data = numpy.delete(self.data, frame, 0) 
 
     def _min_to_sec(self, minutes):
         return minutes*60
 
     def _sec_to_min(self, seconds):
         return seconds/60.0 
+
+    def _check_frame(self, frame):
+        return frame.shape[1] == self.col_num 
+                and frame.shape[0] == 1    
+                and frame[3] == frame[2] - frame[1] 
 
     def _validate_frames(self):
         """ 
@@ -32,7 +46,21 @@ class FrameTime:
         0, 1 indexing (fix?)
         overlapping timing, bad durations
         """
-        pass
+        curr = 0
+        curr_time = 0
+        for i in enumerate(a.shape[0]):
+            frame = self.data[i] 
+            if frame[0] < 0:
+                raise Error("Negative frame number") #make Error classes
+            if frame[0] < curr:
+                raise Error("Frames out of order") 
+            curr = frame[0]
+            if frame[1] < curr_time:
+                raise Error("Overlapping frames") 
+            curr_time = frame[1]
+            if not self._check_frame(frame)
+                raise Error("Bad frame")
+        return True
 
     def get_units(self):
         """Return current units of timing array"""
@@ -48,26 +76,17 @@ class FrameTime:
 
     def from_csv(self, csv_filename, units = 'sec'): 
         """Pulls timing info from csv and stores in an array"""
-        infile = open(csv_filename, 'r')
-        reader = csv.reader(infile)
-        header = reader.readline()
-        self.units = units 
-        for row in reader:
-            self._append_frame(frame)
-        self.data = np.remove(self.data, 0, 1)
-
-    def _entry_to_frame(self, row):
-        frame = np.array(row)
-        frame = frame.reshape((self.data_size, 1))
-        return frame
-
-    def _append_frame(self, frame):
-        if self._check_frame(frame):
-            self.data = np.hstack((self.data, frame)) 
-            
-    def _check_frame(self, frame):
-        return self.data.shape[0] == self.data_size 
-                and self.data.shape[1] == 1    
+        try:
+            infile = open(csv_filename, 'r')
+            reader = csv.reader(infile)
+            header = reader.readline()
+            if header[0].isdigit():
+                head = 0
+            else:
+                head = 1
+            self.data = np.loadtxt(csv_filename, delimiter = ',', skiprows = head)
+        except:
+            raise IOError("Error reading file. Check if file exists or if file is blank")
 
     def from_excel(excel_file, units = 'sec'):
         """Pulls timing info from excel file and stores in an array"""
@@ -81,4 +100,5 @@ class FrameTime:
 
     def get_array(units = 'sec'):
         """Return timing info as numpy array"""
+        return self.data
 
