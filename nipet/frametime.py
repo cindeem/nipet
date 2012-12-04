ff905664ef14dc008fa9fb6109d5f69fe1b51aec
Caleb Wang <cw@berkeley.edu>
2012-12-04 12:55:40 -0800

    Updated docstrings, frametime.py

--- nipet/frametime.py
+++ nipet/frametime.py
@@ -27,24 +27,31 @@ class FrameTime:
         self.data = numpy.delete(self.data, frame, 0) 
 
     def _min_to_sec(self, minutes):
+        """converts minutes to seconds, 
+        assuming that input is a number representing minutes"""
         return minutes*60
 
     def _sec_to_min(self, seconds):
+        """converts seconds to minutes, 
+        assuming that input is a number representing seconds"""
         return seconds/60.0 
 
     def _check_frame(self, frame):
+        """Checks a frame (1x4 array) for the proper shape,
+        and if the duration is equal to stop_time - start_time."""
         return frame.shape[1] == self.col_num 
                 and frame.shape[0] == 1    
                 and frame[3] == frame[2] - frame[1] 
 
     def _validate_frames(self):
         """ 
-        all frames in order
-        frames in order- missing frames
-        frames out of order -exception
-        negative frames - exception
-        0, 1 indexing (fix?)
-        overlapping timing, bad durations
+        Possible cases:
+            all frames in order 
+            frames in order- warning: missing frames
+            frames out of order - exception
+            negative frames - exception
+            0, 1 indexing (fix?)
+            overlapping timing, bad durations - exception
         """
         curr = 0
         curr_time = 0
@@ -54,12 +61,16 @@ class FrameTime:
                 raise Error("Negative frame number") #make Error classes
             if frame[0] < curr:
                 raise Error("Frames out of order") 
+            if frame[0] != curr + 1:
+                missing_frames = True
             curr = frame[0]
             if frame[1] < curr_time:
                 raise Error("Overlapping frames") 
             curr_time = frame[1]
             if not self._check_frame(frame)
                 raise Error("Bad frame")
+        if missing_frames:
+            print "Missing frames"
         return True
 
     def get_units(self):
@@ -68,6 +79,8 @@ class FrameTime:
             return self.units
 
     def generate_empty_protocol(self, frame_num):
+        """Generates empty csv/excel file with header for frametimes,
+        which can then be imported by this class."""
         pass
 
     def from_ecat(self, ecat_file):
