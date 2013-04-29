39960c78020e0d68cdee512f5c1ab1c1a97099e6
Caleb Wang <cw@berkeley.edu>
2013-04-29 13:10:22 -0700

    NF: internal ROI features, importing features

--- nipet/roi.py
+++ nipet/roi.py
@@ -1,75 +1,117 @@
 import numpy as np
 import nibabel as ni
+from scipy.ndimage import affine_transform
 
 """
 Note that np masked arrays use True to indicate that a value is masked.
 This is converted to the more intuitive False to indicate that a value is masked,
 but the former is represented internally, so be careful of using that
 """
-def reslice_mask(data, mask):
+def data_4d(data_block, mask_file):
     """
-    Reslices a mask to data
+    Does stuff with data in a 4d data block
     """
-    pass
+    for frame in data_block:
+        mask = reslice_mask(frame, mask_file)
+        mask_array(frame, mask)
 
-def mask_array(data, mask):
+def frame_data(frame, mask_file, fill_value):
+    """
+    applies mask to frame,
+    returns vector of relevant data points
+    """
+    img = ni.load(frame) 
+    data = img.get_data()
+    mask = reslice_mask(frame, mask_file)
+    return extract_values(data, mask, fill_value = fill_value)
+    
+def frame_stats(frame, mask_file):
+    """
+    applies mask to frame,
+    returns mean and std of relevant data
+    """
+    data = data_from_frame(frame, mask_file, fill_value = fill_value)
+    return mean(data), std(data)
+
+def apply_mask_to_frame(frame, mask_file): 
+    """
+    Applies mask to frame,
+    returns a nifti image with the masked data and same affine
+    """
+    img = ni.load(frame) 
+    data = img.get_data()
+    mask = reslice_mask(frame, mask_filerame, mask_file)
+    new_affine = img.affine()
+    new_data = apply_mask(data, mask, fill_value=0)
+    return Nifti1Image(new_data, new_affine)
+
+def reslice_mask(data_file, mask_file, order = 0):
+    """
+    Reslices a mask to data, given
+    a file defining the mask and a file defining the data.
+    """
+    img = ni.load(data_file)
+    mask_img = ni.load(mask_file)
+    transform = np.eye(4)
+    new_transform = np.dot(np.linalg.inv(mask_img.get_affine()),
+                           np.dot(transform, img.get_affine()))
+
+    new_mask = affine_transform(mask_img.get_data().squeeze(),
+                                new_transform[:3, :3],
+                                offset = new_transform[3, :3],
+                                output_shape = img.get_shape()[:3],
+                                order = order)
+    return new_mask
+
+def mask_array(data, mask, fill_value = 0):
     """
     Given 3-D data and a mask, returns a masked array
     with the mask applied to the data.
+    Assumes that the mask is (1 = valid), so it inverts the mask
     """
     if data.shape != mask.shape:
-        mask = reslice_mask(data, mask)
-    mask = convert_mask(mask)
-    m_array = np.ma.MaskedArray(data, mask=mask)
+        raise Exception('data and mask shape don\'t match')
+    mask = invert_mask(mask)
+    m_array = np.ma.MaskedArray(data, mask=mask, fill_value=fill_value)
     return m_array
 
-def apply_mask(data, mask):
+def apply_mask(data, mask, fill_value):
     """
     Returns data with mask applied.
     """
-    return mask_array(data, mask).data
+    return mask_array(data, mask, fill_value).filled()
 
-def retrieve_values(data, mask):
+def extract_values(data, mask, fill_value):
     """
     Given 3-D data and a mask, return the values from the data
     after mask has been applied in a vector
     """
-    m_arr = mask_array(data, mask)
-    return m_arr[m_data.mask < 1].data
+    return mask_array(data, mask, fill_value).compressed()
 
-def calc_mean_std(data):
+def mean(data):
     """
-    Given a vector of data, calculate mean/stddev
+    Given a vector of data, calculate mean
     """
-    return np.mean(data), np.std(data)
+    return np.mean(data)
 
-def convert_mask(mask):
-    """Converts a mask into desired format.
+def std(data):
     """
-    pass
+    Given a vector of data, calculate mean
+    """   
+    return np.std(data)
 
-def _to_boolean(self, mask):
+def convert_mask(mask, type):
     """
-    Converts int or float mask to boolean
+    Convert mask to datatype type:
+        int
+        bool
     """
-    z = np.zeros(mask.shape)
-    return mask > z
-
-def _to_binary(self, mask):
-    """
-    Converts float or boolean mask to int
-    """
-    return np.array(mask, dtype=int)
+    return np.array(mask, dtype = type)
 
 def invert_mask(mask):
     """
     inverts a mask
-    """
-    pass
-
-def mask_to_ismasked(mask):
-    """
-    takes mask (1 = keep value), inverts to ismasked sense (1 = masked)
+    returns a boolean mask
     """
     return mask < 1
 
---
+++ nipet/tests/test_roi.py
@@ -0,0 +1,29 @@
+from unittest import TestCase, skipIf, skipUnless
+import numpy as np
+from numpy.testing import (assert_raises, assert_equal, assert_almost_equal)
+from os.path import exists, join, split, abspath
+import os
+from .. import roi
+
+class TestROI(TestCase):
+    
+    def test_mask_array(self):
+        raise Exception('not implemented')
+
+    def test_apply_mask(self):
+        raise Exception('not implemented')
+
+    def extract_values(self):
+        raise Exception('not implemented')
+
+    def test_reslice_mask(self):
+        raise Exception('not implemented')
+
+    def test_frame_data(self):
+        raise Exception('not implemented')
+    
+    def test_frame_stats(self):
+        raise Exception('not implemented')
+
+    def test_data_4d(self):
+        raise Exception('not implemented')
